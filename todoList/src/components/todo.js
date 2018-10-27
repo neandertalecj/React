4372328aa9a62eae0894b878c4222dcd41c4cb20
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-10-27 10:05:43 +0300

    some events to TodoList

--- todoList/src/components/todo.js
+++ todoList/src/components/todo.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Todo extends Component {
     render() { 
         return (
-            <div>
-                {/* Im here */}
-                {this.props.task.text}
+            <div >
+                <span>{this.props.task.text}</span>
+                <input type="button" onClick={() => this.props.onDelete(this.props.task.id)} value="x"/>
             </div>
         )
     }
--- todoList/src/components/todoList.js
+++ todoList/src/components/todoList.js
@@ -3,29 +3,42 @@ import Todo from './todo'
 
 class TodoList extends Component {
     state = {
-        list: [
-            {id: 1, text: 'Hello', lineThrough: false}
-        ]
+        list: []
     }
     render() { 
         return ( 
-            <div>
-                <input type="text"/>
-                <button onClick={this.handlerAdd}>Ok</button>
+            <form onSubmit={this.handlerAdd}>
+                <input type="text" id="inp"/>
+                <button submit="submit">Ok</button>
                 {/* <p>Hello </p> */}
                 {/* <Todo list={this.state.list} /> */}
                 <ul>
-                    {this.state.list.map(task => <li key={task.id}>{task.text}</li>)}
+                    {this.state.list.map(task => 
+                        <li key={task.id}>
+                            {/* {task.text} */}
+                            <Todo task={task} 
+                                  onDelete={this.handleDelete} />
+                        </li>)
+                    }
                 </ul>
-            </div>
+            </form>
          )
     }
     handlerAdd = (e) => {
+        const text = document.getElementById('inp').value
         const temp = this.state.list
         const index  = temp.length + 1
-        temp.push({id: index, text: e.target.value, lineThrough: false})
-        console.log(temp)
+        temp.push({id: index, text: text, lineThrough: false})
+
         this.setState({list: temp})
+
+        e.preventDefault()
+        e.target.reset()
+    }
+
+    handleDelete = (id) => {
+        const list = this.state.list.filter(task => task != id )
+        this.setState({list})
     }
 }
  

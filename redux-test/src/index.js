95e3d454ac9331839f39ef64c719f7a790c532dd
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-11-16 18:49:01 +0200

    From "state" to the browser

--- redux-test/src/index.js
+++ redux-test/src/index.js
@@ -23,17 +23,37 @@ function playlist(state = [], action) {
 
 const store = createStore(playlist)
 
+const addTrackBtn = document.querySelectorAll('.addTrack')[0]
+const trackInput = document.querySelectorAll('.trackInput')[0]
+const list = document.querySelectorAll('.list')[0]
+
 // console.log(store.getState())
 store.subscribe(() => {
     console.log('subscribe', store.getState())
+    list.innerHTML = ''
+    trackInput.value = ''
+    store.getState().forEach(track => {
+        const li = document.createElement('li')
+        li.textContent = track
+        list.appendChild(li)
+    })
 })
 
-store.dispatch({
-    type: 'ADD_TRACK',
-    payload: 'Smells like spirit'
-})
+// store.dispatch({
+//     type: 'ADD_TRACK',
+//     payload: 'Smells like spirit'
+// })
 
-store.dispatch({
-    type: 'ADD_TRACK',
-    payload: 'Enter Spandman'
+// store.dispatch({
+//     type: 'ADD_TRACK',
+//     payload: 'Enter Spandman'
+// })
+
+addTrackBtn.addEventListener('click', () => {
+    const trackName = trackInput.value
+    console.log('trackName', trackName)
+    store.dispatch({
+        type: 'ADD_TRACK',
+        payload: trackName
+    })
 })

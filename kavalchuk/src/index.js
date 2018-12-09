6d72b0fd397a275ed0b61b8c82058e3846620696
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-12-09 21:59:58 +0200

    "Kavalchuk". event

--- kavalchuk/src/index.js
+++ kavalchuk/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import {Lesson, Counter, Button} from './Lesson';
+// import {Lesson, Counter, Button} from './Lesson';
+import App from './Lesson2'
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson child={<Button />}><Counter /></Lesson>, document.getElementById('root'));
+ReactDOM.render(
+    // <Lesson child={<Button />}><Counter /></Lesson>,
+    <App />,
+    document.getElementById('root')
+ );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
